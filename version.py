2c28e3b39f3b656c0b2e954abc79968bff93f4a9
blackCheetah <patrik.dvorakk@gmail.com>
2017-09-14 20:10:57 +0200

    ver 3.0.2

---
+++ version.py
@@ -0,0 +1,8 @@
+__author__ = "blackCheetah"
+__copyright__ = "Copyright 2017, All Rights Reserved"
+__credits__ = ["blackCheetah"]
+__license__ = "MIT"
+__version__ = "3.0.2"
+__maintainer__ = "blackCheetah"
+__email__ = ""
+__status__ = "Production"
