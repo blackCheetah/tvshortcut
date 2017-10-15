c318657b60ace068fd12c0b458ebe7dca6b9f56f
blackCheetah <patrik.dvorakk@gmail.com>
2017-10-15 13:24:16 +0200

    useless file, for testing purposes only

--- locked_computer_test.py
+++
@@ -1,9 +0,0 @@
-import time
-from datetime import datetime
-
-for i in range(0, 1000):
-    now = datetime.now()
-
-    print( '{}'.format(i), '\t', '{}'.format(now), flush=True)
-
-    time.sleep(5)
