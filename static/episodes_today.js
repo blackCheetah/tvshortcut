a20ec2234a4d58e62b4b5881fe8dd3216f59ea43
blackCheetah <patrik.dvorakk@gmail.com>
2017-10-20 21:33:59 +0200

    correctly display episodes released for current day

--- static/episodes_today.js
+++ static/episodes_today.js
@@ -1,4 +1,14 @@
 
+// Extending string fucntion
+// E.g. '{}'.format(string)
+String.prototype.format = function () {
+    var args = [].slice.call(arguments);
+    return this.replace(/(\{\d+\})/g, function (a){
+        return args[+(a.substr(1,a.length-2))||0];
+    });
+};
+
+
 function episodesReleasedToday() {
 
     var today = new Date()
@@ -13,24 +23,44 @@ function episodesReleasedToday() {
 
     let element
     let elementRegex = /(\d){0,2}\.(\d){0,2}\./g;
+    //let elementRegex = /16\.10\./g
 
     // for some reason we have to reverse the loop, otherwise every 2nd "td" element will be skipped.. wtf?
     for (var i = elements.length - 1 ; i >= 0; i--) {
         element = elements[i].innerHTML
+
+        //console.log(element)
+
         var match = elementRegex.exec(element)
+        
+        console.log(match)
 
         if (match != null ) {
 
             var episode_date = match[0]
+
+            //console.log(match)
 
             if (episode_date == current_date) {
 
                 todays_episode_element = elements[i].parentNode.innerHTML
                 todays_episode_node = elements[i].parentNode
 
+                //console.log(todays_episode_element)
+                //console.log(todays_episode_node)
+
+                var tvshow_name = todays_episode_node.getElementsByTagName('a').title
+
+                //var tvshow_name = "test"
+
                 // Hotfix for duplicate elemenets to be skipped. 
                 // kinda hacky way, but hey.. it works :-)
-                if (todays_episode_node.parentNode.id == 'snippet--episodes tvshows_today') {
+                var td_id = 'snippet--episodes {0}'.format(tvshow_name)
+
+                //console.log("td_id: ", td_id)
+                //console.log("parentNode.id: ", todays_episode_node.parentNode.id)
+
+                if (todays_episode_node.parentNode.id == td_id) { // check comment on line 48
                     continue;
                 }
 
@@ -45,7 +75,7 @@ function episodesReleasedToday() {
                 var table_element = document.createElement('table')
                 table_element.className = 'episodes'
                 var tbody_element = table_element.createTBody()
-                tbody_element.id = 'snippet--episodes tvshows_today'
+                tbody_element.id = 'snippet--episodes {0}'.format(tvshow_name) // should contain name of the episode as ID
 
                 tbody_element.insertAdjacentHTML('afterbegin', todays_episode_element)
                 div_tvshows_today.appendChild(table_element)
