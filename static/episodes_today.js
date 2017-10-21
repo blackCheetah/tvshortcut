b3f980da4f21205dcfb461ac609ba798a5cf27ba
blackCheetah <patrik.dvorakk@gmail.com>
2017-10-21 12:46:05 +0200

    added forEach instead of for loop, for all episodes to be visible
    - forEach will go through all "td" elements in html code in every node,
    instead of skipping some nodes like with the reverse "for" loop.
    - marginBottom will be changed to 15% if any episode was released on
    current day
    - added nthParent function to find parentNode of any given element
    - added links/credit to source codes on stackoverflow, as I didn't write
    everything in this code by myself :-)

--- static/episodes_today.js
+++ static/episodes_today.js
@@ -1,6 +1,11 @@
+/*********************************************************** 
+    On homepage, show episodes which were released today
+
+************************************************************/
 
 // Extending string fucntion
 // E.g. '{}'.format(string)
+// source code: https://stackoverflow.com/a/25227174
 String.prototype.format = function () {
     var args = [].slice.call(arguments);
     return this.replace(/(\{\d+\})/g, function (a){
@@ -8,7 +13,15 @@ String.prototype.format = function () {
     });
 };
 
+// source code: https://stackoverflow.com/a/35130223
+function nthParent(element, n) {
+    while(n-- && element)  
+        element = element.parentNode;
+    return element;
+}
 
+// Check which episodes were released today and create html elements
+// to show them on homepage
 function episodesReleasedToday() {
 
     var today = new Date()
@@ -16,61 +29,59 @@ function episodesReleasedToday() {
     var mm = today.getMonth()+1
 
     current_date = dd + '.' + mm + '.'
-
     console.log("Current date: " + current_date)
 
     let elements = document.getElementsByTagName("td")
+    let arr = [].slice.call(elements)
 
-    let element
+    // Date format: 5.10., 10.10., 21.10. etc.
     let elementRegex = /(\d){0,2}\.(\d){0,2}\./g;
-    //let elementRegex = /16\.10\./g
+    
+    // arr.forEach(function(element){
+    //     elementHtml = element.innerHTML
+    //     console.log(elementHtml)
+    // });
 
-    // for some reason we have to reverse the loop, otherwise every 2nd "td" element will be skipped.. wtf?
-    for (var i = elements.length - 1 ; i >= 0; i--) {
-        element = elements[i].innerHTML
-
-        //console.log(element)
-
-        var match = elementRegex.exec(element)
-        
-        console.log(match)
+    // Go through all of the "td" elements in html code (array)
+    arr.forEach(function(item) {
+        let element = item.innerHTML
+        let match = elementRegex.exec(element)
 
         if (match != null ) {
 
+            // Find the date when episode was released
             var episode_date = match[0]
 
-            //console.log(match)
+            // Find class name of TV show which doesnt have any upcoming episodes
+            var elementOuter = nthParent(item, 4).className //item.parentNode.parentNode.parentNode.parentNode.className
+            var elementID = nthParent(item, 4).id //item.parentNode.parentNode.parentNode.parentNode.className
 
             if (episode_date == current_date) {
 
-                todays_episode_element = elements[i].parentNode.innerHTML
-                todays_episode_node = elements[i].parentNode
-
-                //console.log(todays_episode_element)
-                //console.log(todays_episode_node)
+                todays_episode_element = item.parentNode.innerHTML
+                todays_episode_node = item.parentNode
 
                 var tvshow_name = todays_episode_node.getElementsByTagName('a').title
-
-                //var tvshow_name = "test"
 
                 // Hotfix for duplicate elemenets to be skipped. 
                 // kinda hacky way, but hey.. it works :-)
                 var td_id = 'snippet--episodes {0}'.format(tvshow_name)
 
-                //console.log("td_id: ", td_id)
-                //console.log("parentNode.id: ", todays_episode_node.parentNode.id)
-
-                if (todays_episode_node.parentNode.id == td_id) { // check comment on line 48
-                    continue;
+                // If episode is already on homepage, continue with forEach loop
+                if (todays_episode_node.parentNode.id == td_id) {
+                    return;
                 }
 
-                var title = todays_episode_node.parentNode.parentNode.parentElement.getElementsByTagName('h2')[0].textContent //.getElementsByTagName('h2')[0].innerHTML
+                // Get name of the TV Show
+                var title = nthParent(todays_episode_node, 3).getElementsByTagName('h2')[0].textContent
 
+                // Create html elements for TV shows' episode
                 var h2_element = document.createElement('h2')
                 var h2_title_node = document.createTextNode(title)
                 h2_element.appendChild(h2_title_node)
                 var div_tvshows_today = document.getElementById('tvshows_today')
                 div_tvshows_today.appendChild(h2_element)
+                div_tvshows_today.style.marginBottom = '15%'
 
                 var table_element = document.createElement('table')
                 table_element.className = 'episodes'
@@ -80,9 +91,8 @@ function episodesReleasedToday() {
                 tbody_element.insertAdjacentHTML('afterbegin', todays_episode_element)
                 div_tvshows_today.appendChild(table_element)
             }
-
         }
-    }
+    });
 
 }
 
